1ef122057f8bed4e840f4f575b2318cf739a66de
cegrif01 <cegrif01@gmail.com>
2013-03-08 22:48:11 -0500

    7.2 Signup form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase) #since email addresses are unique, this md5 will be too!
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt:user.name, class: "gravatar")
+	end
 end
